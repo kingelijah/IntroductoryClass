38eb481ae09c21ad165d91b59d49db326303517a
ElijahAleakhue <elijah.aleakhue@thebulb.africa>
2021-04-18 18:08:20 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IntroductoryClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               
+          string values = "";
+          string.IsNullOrEmpty(values);
+         //Console.WriteLine(values.Replace(" ",""));
+              
+          
+        }
+    }
+}
+
